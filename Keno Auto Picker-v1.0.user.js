6c3a7730ce71dd7e1eec2ef1bc39d1c2cefa4baf
PainfullySad <141632454+PainfullySad@users.noreply.github.com>
2025-03-25 21:17:36 +0100

    Add files via upload

---
+++ Keno Auto Picker-v1.0.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         Keno Auto Picker
+// @version      v1.0
+// @description  Automatically selects the keno picks when accessing the grarrly keno & bets the max bet
+// @author       Table (UN: _mega_blast_erke_)
+// @match        *.neopets.com/prehistoric/keno.phtml
+// @icon         https://www.neopets.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+// Wait for the page to load
+$(document).ready(function() {
+
+    // Do you want to use the Quick Pick to randomize the selections by the website?
+    // If yes, change the next line to true
+    var randomize = false;
+
+    if (randomize) // Randomize mechanism (uses the same function as the quick pick button)
+    {
+        random_eggs(10);
+    }
+    else // Personal Pick mechanism
+    {
+        // Change these to your preferred picks
+        var picks = [8, 16, 26, 27, 35, 40, 47, 52, 66, 73];
+
+        // Click on the eggs
+        picks.forEach(function(digit) {
+            $('input[name="ch' + digit + '"]').trigger('click');
+            $('input[name="ch' + digit + '"]').trigger('onclick');
+        });
+    }
+    // Do you want to bet the maximum amount that you can?
+    // If no, change the next line to false and add the amount you want to => var desiredBet = {your desired amount};
+    var maxBet = true;
+    var desiredBet = 0;
+
+    if (maxBet) {
+        // Find the max Bet on your page
+        var PossibleElementsWithMaxBet = $('.content').find('b');
+        PossibleElementsWithMaxBet.each(function() {
+            var element = $(this);
+            if (element.text().includes(" NP")) {
+                desiredBet = element.text().replace(" NP", "")
+            }
+        });
+    }
+
+    // Inputs your bet
+    $('input[name="bet"]').val(desiredBet);
+});
